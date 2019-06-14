955adb5092ac4c77b52f82ed25bcd6902f5ba1de
Shaw Vrana <shaw@vranix.com>
2019-06-14 15:55:18 -0700

    v0.1.9-alpha (#1)

--- Formula/wish-dev.rb
+++ Formula/wish-dev.rb
@@ -2,9 +2,9 @@ class WishDev < Formula
   desc "Docker-compose wrapper for development environments"
   homepage "https://github.com/wish/dev"
 
-  url "https://github.com/wish/dev/releases/download/v0.1.8-alpha/dev.darwin"
-  version "0.1.8-alpha"
-  sha256 "8ae2c2a20d77e62c01c209fd7555df3d60979aee9bb22e88d7ad772683d7f119"
+  url "https://github.com/wish/dev/releases/download/v0.1.9-alpha/dev.darwin"
+  version "0.1.9-alpha"
+  sha256 "4414318a9ad3efeef0f8102d23b92bf4b540cc664e4e56e807fb2129b4c5ec50"
 
   def install
     bin.install "dev.darwin" => "dev"
