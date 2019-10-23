33c757eb9090d1ba9bbf96d8eb25d70e99b09e89
Zelin L <16577660+zelin-l@users.noreply.github.com>
2019-10-22 17:47:47 -0700

    Update wish-ctl.rb

--- Formula/wish-ctl.rb
+++ Formula/wish-ctl.rb
@@ -2,7 +2,7 @@ class WishCtl < Formula
   desc "Concurrent multi-cluster Kubernetes utility and management tool"
   homepage "https://github.com/wish/ctl"
   url "https://github.com/wish/ctl/releases/download/v12.0.1-1/ctl_12.0.1-1_darwin_amd64"
-  sha256 "d3bfd895df59373f3bc79e7aaab4aa3f06220a641f8903de4afc4a8a7c87eae0"
+  sha256 "afe310481731c4932282700a78a2e502a46e2210a1df3982ca7e0bb618a83518"
 
   def install
     bin.install "ctl_12.0.1-1_darwin_amd64" => "ctl"
