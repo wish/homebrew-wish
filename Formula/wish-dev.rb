b9eac62e522f1b8e21d0987fd5f3189d1eadbf14
Leif Huhn <lhuhn@contextlogic.com>
2020-05-11 16:53:49 -0500

    Actually build dev on macOS, and bump the version

--- Formula/wish-dev.rb
+++ Formula/wish-dev.rb
@@ -2,12 +2,13 @@ class WishDev < Formula
   desc "Docker-compose wrapper for development environments"
   homepage "https://github.com/wish/dev"
 
-  url "https://github.com/wish/dev/releases/download/v0.1.9-alpha/dev.darwin"
-  version "0.1.9-alpha"
-  sha256 "4414318a9ad3efeef0f8102d23b92bf4b540cc664e4e56e807fb2129b4c5ec50"
+  url "https://github.com/wish/dev", :using => :git, :tag => "20200414"
+
+  depends_on "go@1.12" => :build
 
   def install
-    bin.install "dev.darwin" => "dev"
+    system "make"
+    bin.install "build/dev.darwin" => "dev"
   end
 
   test do
