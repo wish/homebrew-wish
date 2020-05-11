687b6ddbb5f4599a4a9c8383999f05d45ed03569
Leif Huhn <lhuhn@contextlogic.com>
2020-05-11 16:52:52 -0500

    Add formula for dobi

---
+++ Formula/dobi.rb
@@ -0,0 +1,12 @@
+class Dobi < Formula  desc "A build automation tool for Docker applications"
+  homepage "https://dnephin.github.io/dobi/"  url "https://github.com/dnephin/dobi/releases/download/v0.13.0/dobi-Darwin"
+  sha256 "1414519d1cfced5f7f731527f5ad0d0d10d9fc05cad8fffe5e912be3190cae19"
+
+  def install
+    bin.install "dobi-Darwin" => "dobi"
+  end
+
+  test do
+    system "#{bin}/dobi", "--version"
+  end
+end
