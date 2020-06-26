061841476f4f68ab34f6ae9f6ce343b9054d4359
Leif Huhn <lhuhn@contextlogic.com>
2020-06-26 13:37:32 -0500

    Clear GOCACHE and GOPATH when building packr2

--- Formula/wish-ctl.rb
+++ Formula/wish-ctl.rb
@@ -8,8 +8,7 @@ class WishCtl < Formula
   def install
     ENV["CGO_ENABLED"] = "0"
     ENV["GOOS"] = "darwin"
-    system "go", "get", "-u", "github.com/gobuffalo/packr/v2/packr2"
-    system "sh", "-c", "$HOME/go/bin/packr2"
+    system "sh", "-c", "GOCACHE= GOPATH= go get -v -u github.com/gobuffalo/packr/v2/packr2; $HOME/go/bin/packr2"
     system "go", "build", "-o", "bin/darwin/ctl", "github.com/wish/ctl"
     bin.install "bin/darwin/ctl" => "ctl"
   end
