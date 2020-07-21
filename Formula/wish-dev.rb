3fe0dffc1a25e90dea67713c708d739f12e333bc
Leif Huhn <lhuhn@contextlogic.com>
2020-07-21 10:47:18 -0500

    Compile dev with go 1.13

--- Formula/wish-dev.rb
+++ Formula/wish-dev.rb
@@ -2,9 +2,9 @@ class WishDev < Formula
   desc "Docker-compose wrapper for development environments"
   homepage "https://github.com/wish/dev"
 
-  url "https://github.com/wish/dev", :using => :git, :tag => "20200414"
+  url "https://github.com/wish/dev", :using => :git, :tag => "20200721"
 
-  depends_on "go@1.12" => :build
+  depends_on "go@1.13" => :build
 
   def install
     system "make"
