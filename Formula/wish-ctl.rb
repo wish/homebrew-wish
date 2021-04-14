466f88786a99da443e14e70e54f42df9c252d8cc
Weilu Jia <wjia@wish.com>
2021-04-14 13:09:56 -0700

    `brew style --fix`
    Removes deprecation warnings

--- Formula/wish-ctl.rb
+++ Formula/wish-ctl.rb
@@ -1,12 +1,12 @@
 class WishCtl < Formula
   desc "Concurrent multi-cluster Kubernetes utility and management tool"
   homepage "https://github.com/wish/ctl"
-  url "https://github.com/wish/ctl", :using => :git, :tag => "v14.3.0"
+  url "https://github.com/wish/ctl", using: :git, tag: "v14.3.0"
 
   depends_on "go@1.16" => :build
-  depends_on "wget"
   depends_on "git"
   depends_on "kubectl"
+  depends_on "wget"
 
   def install
     ENV["CGO_ENABLED"] = "0"
--- Formula/wish-dev.rb
+++ Formula/wish-dev.rb
@@ -2,7 +2,7 @@ class WishDev < Formula
   desc "Docker-compose wrapper for development environments"
   homepage "https://github.com/wish/dev"
 
-  url "https://github.com/wish/dev", :using => :git, :tag => "20200721"
+  url "https://github.com/wish/dev", using: :git, tag: "20200721"
 
   depends_on "go@1.16" => :build
 
