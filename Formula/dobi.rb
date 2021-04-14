d4a4a278fae9faffca964e3ffff86d5eb51eac28
Weilu Jia <wjia@wish.com>
2021-04-14 13:09:56 -0700

    `brew style --fix`
    Removes deprecation warnings

--- Formula/dobi.rb
+++ Formula/dobi.rb
@@ -1,5 +1,5 @@
 class Dobi < Formula
-  desc "A build automation tool for Docker applications"
+  desc "Build automation tool for Docker applications"
   homepage "https://dnephin.github.io/dobi/"
   url "https://github.com/dnephin/dobi/releases/download/v0.13.0/dobi-Darwin"
   sha256 "1414519d1cfced5f7f731527f5ad0d0d10d9fc05cad8fffe5e912be3190cae19"
