0280a468d5991c9250f8ec0bb3e860fab48096c5
axia-wish <axia@wish.com>
2022-05-18 23:09:18 -0700

    Update formula for dobi and dev to use single build

--- Formula/wish-dev.rb
+++ Formula/wish-dev.rb
@@ -2,13 +2,13 @@ class WishDev < Formula
   desc "Docker-compose wrapper for development environments"
   homepage "https://github.com/wish/dev"
 
-  url "https://github.com/wish/dev", using: :git, tag: "20200721"
+  url "https://github.com/wish/dev", using: :git, tag: "20220516"
 
   depends_on "go@1.16" => :build
 
   def install
     system "make"
-    bin.install "build/dev.darwin" => "dev"
+    bin.install "build/dev" => "dev"
   end
 
   test do
