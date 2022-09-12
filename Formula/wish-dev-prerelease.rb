c9814e0cb4f6873ee9fd9ec529b78892bdda35b1
Leif Huhn <lhuhn@contextlogic.com>
2022-09-12 16:29:07 -0400

    Add wish-dev-prerelease for installing dev from master branch

--- Formula/wish-dev-prerelease.rb
+++ Formula/wish-dev-prerelease.rb
@@ -1,10 +1,11 @@
-class WishDev < Formula
+class WishDevPrerelease < Formula
   desc "Docker-compose wrapper for development environments"
   homepage "https://github.com/wish/dev"
+  version "master"
 
-  url "https://github.com/wish/dev", using: :git, tag: "20220516"
+  url "https://github.com/wish/dev", using: :git, branch: "master"
 
-  conflicts_with "wish-dev-prerelease", because: "Can only install one dev binary"
+  conflicts_with "wish-dev", because: "Can only install one dev binary"
 
   depends_on "go@1.16" => :build
 
