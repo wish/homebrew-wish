afc164a27039873e8b3be9b01cd471874b7e8e09
Leif Huhn <lhuhn@contextlogic.com>
2022-09-28 18:42:29 -0400

    Release new dev tool

--- Formula/wish-dev.rb
+++ Formula/wish-dev.rb
@@ -2,7 +2,7 @@ class WishDev < Formula
   desc "Docker-compose wrapper for development environments"
   homepage "https://github.com/wish/dev"
 
-  url "https://github.com/wish/dev", using: :git, tag: "20220516"
+  url "https://github.com/wish/dev", using: :git, tag: "20220923"
 
   conflicts_with "wish-dev-prerelease", because: "Can only install one dev binary"
 
