f567f372ddfde72b08ca59584afdf541839ca0ad
Leif Huhn <lhuhn@contextlogic.com>
2022-10-06 11:58:11 -0400

    Docker for Mac 4.12.0 (was 4.4.2)

--- Casks/docker-wish.rb
+++ Casks/docker-wish.rb
@@ -1,20 +1,16 @@
-cask "docker-wish" do
-  arch = Hardware::CPU.intel? ? "amd64" : "arm64"
+cask 'docker-wish' do
+  arch arm: 'arm64', intel: 'amd64'
 
-  version "4.4.2,73305"
-
-  if Hardware::CPU.intel?
-    sha256 "101c573dea42dd8eb07c8af6a6c01fa5508fce8371fa1279ab8d1a71eb6305d7"
-  else
-    sha256 "815bb1965e94f6d7f312cd20046a4dbd07f3db19631557de1d495702ddc298cd"
-  end
+  version '4.12.0,85629'
+  sha256 arm: '7eb63b4819cd1f87c61d5e8f54613692e07fb203d81bcf8d66f5de55489d3b81',
+         intel: '41085009458ba1741c6a86c414190780ff3b288879aa27821fc4a985d229653c'
 
   url "https://desktop.docker.com/mac/main/#{arch}/#{version.csv.second}/Docker.dmg"
-  name "Docker Desktop"
-  name "Docker Community Edition"
-  name "Docker CE"
-  desc "App to build and share containerized applications and microservices"
-  homepage "https://www.docker.com/products/docker-desktop"
+  name 'Docker Desktop'
+  name 'Docker Community Edition'
+  name 'Docker CE'
+  desc 'App to build and share containerized applications and microservices'
+  homepage 'https://www.docker.com/products/docker-desktop'
 
   livecheck do
     url "https://desktop.docker.com/mac/main/#{arch}/appcast.xml"
@@ -32,7 +28,7 @@ cask "docker-wish" do
     kubernetes-cli
   ]
 
-  app "Docker.app"
+  app 'Docker.app'
   binary "#{appdir}/Docker.app/Contents/Resources/etc/docker.bash-completion",
          target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/docker"
   binary "#{appdir}/Docker.app/Contents/Resources/etc/docker-compose.bash-completion",
@@ -46,48 +42,57 @@ cask "docker-wish" do
   binary "#{appdir}/Docker.app/Contents/Resources/etc/docker-compose.fish-completion",
          target: "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/docker-compose.fish"
 
-  uninstall delete:    [
-    "/Library/PrivilegedHelperTools/com.docker.vmnetd",
-    "/usr/local/bin/docker-compose",
-    "/usr/local/bin/docker-credential-desktop",
-    "/usr/local/bin/docker-credential-ecr-login",
-    "/usr/local/bin/docker-credential-osxkeychain",
-    "/usr/local/bin/docker",
-    "/usr/local/bin/hyperkit",
-    "/usr/local/bin/kubectl.docker",
-    "/usr/local/bin/kubectl",
-    "/usr/local/bin/notary",
-    "/usr/local/bin/vpnkit",
-  ],
-            launchctl: [
-              "com.docker.helper",
-              "com.docker.vmnetd",
+  uninstall delete: [
+              '/Library/PrivilegedHelperTools/com.docker.vmnetd',
+              '/usr/local/bin/com.docker.cli',
+              '/usr/local/bin/docker-compose-v1',
+              '/usr/local/bin/docker-compose',
+              '/usr/local/bin/docker-credential-desktop',
+              '/usr/local/bin/docker-credential-ecr-login',
+              '/usr/local/bin/docker-credential-osxkeychain',
+              '/usr/local/bin/docker',
+              '/usr/local/bin/hub-tool',
+              '/usr/local/bin/hyperkit',
+              '/usr/local/bin/kubectl.docker',
+              '/usr/local/bin/kubectl',
+              '/usr/local/bin/notary',
+              '/usr/local/bin/vpnkit',
+              "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_docker",
+              "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_docker_compose",
+              "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/docker.fish",
+              "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/docker-compose.fish",
+              "#{HOMEBREW_PREFIX}/etc/bash_completion.d/docker",
+              "#{HOMEBREW_PREFIX}/etc/bash_completion.d/docker-compose"
             ],
-            quit:      "com.docker.docker"
+            launchctl: [
+              'com.docker.helper',
+              'com.docker.vmnetd'
+            ],
+            quit: 'com.docker.docker'
 
   zap trash: [
-    "/usr/local/bin/docker-compose.backup",
-    "/usr/local/bin/docker.backup",
-    "~/.docker",
-    "~/Library/Application Scripts/com.docker.helper",
-    "~/Library/Application Support/com.bugsnag.Bugsnag/com.docker.docker",
-    "~/Library/Application Support/Docker Desktop",
-    "~/Library/Caches/com.docker.docker",
-    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.docker.docker",
-    "~/Library/Caches/KSCrashReports/Docker",
-    "~/Library/Containers/com.docker.docker",
-    "~/Library/Containers/com.docker.helper",
-    "~/Library/Group Containers/group.com.docker",
-    "~/Library/HTTPStorages/com.docker.docker.binarycookies",
-    "~/Library/Logs/Docker Desktop",
-    "~/Library/Preferences/com.docker.docker.plist",
-    "~/Library/Preferences/com.electron.docker-frontend.plist",
-    "~/Library/Preferences/com.electron.dockerdesktop.plist",
-    "~/Library/Saved Application State/com.electron.docker-frontend.savedState",
-    "~/Library/Saved Application State/com.electron.dockerdesktop.savedState",
-  ],
+        '/usr/local/bin/docker-compose.backup',
+        '/usr/local/bin/docker.backup',
+        '~/.docker',
+        '~/Library/Application Scripts/com.docker.helper',
+        '~/Library/Application Support/com.bugsnag.Bugsnag/com.docker.docker',
+        '~/Library/Application Support/Docker Desktop',
+        '~/Library/Caches/com.docker.docker',
+        '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.docker.docker',
+        '~/Library/Caches/KSCrashReports/Docker',
+        '~/Library/Containers/com.docker.docker',
+        '~/Library/Containers/com.docker.helper',
+        '~/Library/Group Containers/group.com.docker',
+        '~/Library/HTTPStorages/com.docker.docker.binarycookies',
+        '~/Library/Logs/Docker Desktop',
+        '~/Library/Preferences/com.docker.docker.plist',
+        '~/Library/Preferences/com.electron.docker-frontend.plist',
+        '~/Library/Preferences/com.electron.dockerdesktop.plist',
+        '~/Library/Saved Application State/com.electron.docker-frontend.savedState',
+        '~/Library/Saved Application State/com.electron.dockerdesktop.savedState'
+      ],
       rmdir: [
-        "~/Library/Caches/com.plausiblelabs.crashreporter.data",
-        "~/Library/Caches/KSCrashReports",
+        '~/Library/Caches/com.plausiblelabs.crashreporter.data',
+        '~/Library/Caches/KSCrashReports'
       ]
 end
